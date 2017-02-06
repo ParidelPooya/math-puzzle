f8752ef1efd789c9f2c4f1231715db164b9dc29e
DESKTOP-5OOPHQ1\pooya <pooya.p@outlook.com>
2017-02-05 20:13:49 -0800

    option

--- worksheet/main.js
+++ worksheet/main.js
@@ -9,60 +9,114 @@ app.controller('myCtrl', function($scope,$timeout,$interval) {
 
     ct.questions=[];
 
+    ct.type="multiply";
 
-    ct.newNumbers=function() {
-        var r=Math.random();
+    var questions=[];
 
-        if(r>0.5){
-            ct.sign="+";
+    questions.push(function(){
+        ct.sign="+";
 
-            ct.number1 = Math.round(Math.random()*30);
-            if(ct.number1<10)
-                ct.number1="0"+ct.number1;
+        ct.number1 = Math.round(Math.random()*40);
+        if(ct.number1<10)
+            ct.number1="0"+ct.number1;
 
-            ct.number2 = Math.round(Math.random()*30);
+        ct.number2 = Math.round(Math.random()*40);
 
-            if(ct.number2<10)
-                ct.number2="0"+ct.number2;
+        if(ct.number2<10)
+            ct.number2="0"+ct.number2;
 
-            ct.correctAnswer=""+(parseInt(ct.number1,10)+parseInt(ct.number2,10));
+        ct.correctAnswer=""+(parseInt(ct.number1,10)+parseInt(ct.number2,10));
 
-        }
-        else
-        {
-            ct.sign="-";
-
-            var n1 = Math.round(Math.random()*30);
-            var n2 = Math.round(Math.random()*30);
-
-            ct.number1=Math.max(n1,n2);
-            ct.number2=Math.min(n1,n2);
-
-            ct.correctAnswer=""+(parseInt(ct.number1,10)-parseInt(ct.number2,10));
-
-            if(ct.number1<10)
-                ct.number1="0"+ct.number1;
-
-            if(ct.number2<10)
-                ct.number2="0"+ct.number2;
-
-        }
-
-        ct.questions.push({
+        return {
             sign:ct.sign,
             number1:ct.number1,
             number2:ct.number2,
             correctAnswer:ct.correctAnswer
-        });
+        };
+    });
+
+    questions.push(function(){
+        ct.sign="-";
+
+        var n1 = Math.round(Math.random()*50);
+        var n2 = Math.round(Math.random()*40);
+
+        ct.number1=Math.max(n1,n2);
+        ct.number2=Math.min(n1,n2);
+
+        ct.correctAnswer=""+(parseInt(ct.number1,10)-parseInt(ct.number2,10));
+
+        if(ct.number1<10)
+            ct.number1="0"+ct.number1;
+
+        if(ct.number2<10)
+            ct.number2="0"+ct.number2;
+
+        return {
+            sign:ct.sign,
+            number1:ct.number1,
+            number2:ct.number2,
+            correctAnswer:ct.correctAnswer
+        };
+    });
+
+    questions.push(function(){
+        var r=Math.random();
+
+        if(r>0.5){
+            return questions[0]();
+        }
+        else
+        {
+            return questions[1]();
+        }
+
+    });
+
+    questions.push(function(){
+        ct.sign="*";
+
+        ct.number1 = 2+Math.floor(Math.random()*8);
+        ct.number2 = 2+ Math.floor(Math.random()*8);
+
+        ct.correctAnswer=""+(parseInt(ct.number1,10)*parseInt(ct.number2,10));
+
+        return {
+            sign:ct.sign,
+            number1:ct.number1,
+            number2:ct.number2,
+            correctAnswer:ct.correctAnswer
+        };
+    });
+
+    ct.newNumbers=function() {
+
+        switch(ct.type){
+
+            case 'additon':
+                ct.questions.push(questions[0]());
+                break;
+            case 'subtract':
+                ct.questions.push(questions[1]());
+                break;
+            case 'add+sub':
+                ct.questions.push(questions[2]());
+                break;
+            case 'multiply':
+                ct.questions.push(questions[3]());
+                break;
+        }
 
         return;
     }
 
-    for(var ipos=0;ipos<84;ipos++)
-    {
-        ct.newNumbers();
-    }
+    ct.change=function() {
+        ct.questions=[];
 
+        for (var ipos = 0; ipos < 84; ipos++) {
+            ct.newNumbers();
+        }
+    };
 
-
+    ct.change();
 });
